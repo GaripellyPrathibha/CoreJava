920c69ed0886efe2364ecbc788d0dc8b1142b459
PrathibhaGaripelly <115960269+PrathibhaGaripelly@users.noreply.github.com>
2024-01-12 09:43:07 -0600

    usage of abstract class

---
+++ abstractClass/ImpDepartment.java
@@ -0,0 +1,13 @@
+public class ImpDepartment extends Department{
+    int departmentSize;
+    public ImpDepartment(int departmentSize)
+    {
+        this.departmentSize=departmentSize;
+    }
+    @Override
+    public int getDepartmentSize()
+    {
+        return departmentSize;
+    }
+
+}
---
+++ abstractClass/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ImpDepartment dep=new ImpDepartment(410);
+        System.out.println(dep.getDepartmentSize());
+        
+    }
+}
