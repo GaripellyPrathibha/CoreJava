ecfac5965e17882a23c9a5c92a3b281c929cdbc1
PrathibhaGaripelly <115960269+PrathibhaGaripelly@users.noreply.github.com>
2024-01-12 09:47:31 -0600

    overloading concept

---
+++ overloading_concept/Product.java
@@ -0,0 +1,23 @@
+public class Product {
+    public double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice(int quantity)
+    {
+        return quantity*price;
+    }
+    public static void main(String[] args) {
+    Product p=new Product();
+    p.setPrice(2.99);
+    System.out.println(p.getPrice(2));
+    }
+
+
+}
