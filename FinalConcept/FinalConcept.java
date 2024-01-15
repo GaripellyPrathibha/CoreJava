b0783dc38ac6d72b318bc2a8b0fd9e4a2d9e4d64
PrathibhaGaripelly <115960269+PrathibhaGaripelly@users.noreply.github.com>
2024-01-15 13:05:22 -0600

    added final concept files

---
+++ FinalConcept/FinalConcept.java
@@ -0,0 +1,11 @@
+public class FinalConcept {
+    final int finalVariable=67;
+    final void finalVariableMethod()
+    {
+        System.out.println("The value of final variable in final method is "+finalVariable);
+    }
+    public static void main(String[] args){
+        FinalConcept c=new FinalConcept();
+        c.finalVariableMethod();
+    }
+}
