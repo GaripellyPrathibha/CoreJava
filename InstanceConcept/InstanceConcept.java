d506ae3970da031d0b83eaae352fea663e5ad8a9
PrathibhaGaripelly <115960269+PrathibhaGaripelly@users.noreply.github.com>
2024-01-15 22:05:06 -0600

    added code

--- InstanceConcept/InstanceConcept.java
+++
@@ -1,7 +0,0 @@
-package InstanceConcept;
-
-public class InstanceConcept {
-    ChildClass child=new ChildClass(11,12);
-    ChildClass child2=new ChildClass(20);
-
-}
---
+++ InstanceConcept/Main.java
@@ -0,0 +1,12 @@
+package InstanceConcept;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        ChildClass child1=new ChildClass(11,12);
+        System.out.println(child1.cInstanceVariable +" and "+ child1.pInstanceVariable);
+        ChildClass child2=new ChildClass(20);
+        System.out.println(child2.cInstanceVariable + " and "+ child2.pInstanceVariable);
+    }
+
+}
