3aae9cb1bf748ccb8ab249de726f1b2ce317f7a4
PrathibhaGaripelly <115960269+PrathibhaGaripelly@users.noreply.github.com>
2024-01-18 12:54:07 -0600

    Changed directory name

---
+++ operation/Person.java
@@ -0,0 +1,32 @@
+package operation;
+
+import java.util.Scanner;
+
+public class Person {
+
+    public static String findAge(int age)
+    {
+        if(age<13)
+        {
+            return "kid";
+        }
+        else if(age>=13 &&age<=19)
+        {
+            return "teen";
+        }
+
+        return "adult";
+
+
+    }
+    public static void main(String[] args)
+    {
+        int age;
+        Scanner sc= new Scanner(System.in);
+        age=sc.nextInt();
+        System.out.println("The person is "+Person.findAge(age));
+
+    }
+
+
+}
