92fcd1cd1b27f1e899d83f53d9dcbf873ea3e73a
PrathibhaGaripelly <115960269+PrathibhaGaripelly@users.noreply.github.com>
2024-02-09 15:17:18 -0600

    code changed

--- src/main/java/lambdaprograms/DoubleEvenNumbers.java
+++ src/main/java/lambdaprograms/DoubleEvenNumbers.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class DoubleEvenNumbers {
     public static int[] print(int[] num)
     {
-        int[] evenArray=Arrays.stream(num).filter(n->n%2==0).map(n->n+n).toArray();
+        int[] evenArray=Arrays.stream(num).filter(n->n%2!=0).map(n->n+n).toArray();
         return evenArray;
     }
 }
